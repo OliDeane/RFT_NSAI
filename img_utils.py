e37bdaa4d221a1d91d1c21d0b3403413509f3143
OliDeane <oliver.deane@gmail.com>
2022-02-16 16:03:01 +0000

    Changed to collect object lists

--- img_utils.py
+++ img_utils.py
@@ -1,42 +1,73 @@
 import cv2
+import numpy as np
 
+def get_object_position(size, center):
 
-def draw_sCsS(img, positions, color, size = 25):
+    start = (center[0]-size, center[1]-size)
+    end = (center[0]+size, center[1]+size)
+
+    return start, end, center
+
+def draw_sCsS(img, positions, color, color_id, objects, size = 25):
     """Draws the same color same shape pairing"""
 
-    cv2.circle(img,positions[0][0], size, color, -1)
-    cv2.circle(img,positions[0][1], size, color, -1)
+    # Draw objec A and add it to the object list
+    start, end, center = get_object_position(size, center = positions[0][0])
+    cv2.circle(img,center, size, color, -1)
+    objects.append((color_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-    return img
+    # Draw object B and add to th eobject list
+    start, end, center = get_object_position(size, center = positions[0][1])
+    cv2.circle(img,center, size, color, -1)
+    objects.append((color_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-def draw_sCdS(img, positions, color, size = 25):
 
-    rec_center = positions[1][0]
-    start = (rec_center[0]-size, rec_center[1]-size)
-    end = (rec_center[0]+size, rec_center[1]+size)
+    return img, objects
 
+def draw_sCdS(img, positions, color, color_id, objects, size = 25):
+    """Draws the same color different shape pairing"""
+
+    # Draw and add first object 
+    start, end, center = get_object_position(size, center = positions[1][0])
     cv2.rectangle(img, start, end, color, -1)
-    cv2.circle(img,positions[1][1], size, color, -1)
+    objects.append((color_id, center, 'r', (start[0], start[1], end[0], end[1])))
 
 
-    return img
+    # Draw and add second object
+    start, end, center = get_object_position(size, center = positions[1][1])
+    cv2.circle(img,center, size, color, -1)
+    objects.append((color_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-def draw_dCsS(img, positions, color1, color2, size = 25):
+    return img, objects
 
-    cv2.circle(img,positions[2][0], size, color1, -1)
-    cv2.circle(img,positions[2][1], size, color2, -1)
+def draw_dCsS(img, positions, color1, color2, color1_id, color2_id, objects, size = 25):
+    """Draws the different color same shape pairing"""
 
-    return img
+    # Draw and add first object 
+    start, end, center = get_object_position(size, center = positions[2][0])
+    cv2.circle(img,center, size, color1, -1)
+    objects.append((color1_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-def draw_dCdS(img, positions, color1, color2, size = 25):
+    # Draw and add first object 
+    start, end, center = get_object_position(size, center = positions[2][1])
+    cv2.circle(img,center, size, color2, -1)
+    objects.append((color2_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-    rec_center = positions[3][0]
-    start = (rec_center[0]-size, rec_center[1]-size)
-    end = (rec_center[0]+size, rec_center[1]+size)
+    return img, objects
 
+def draw_dCdS(img, positions, color1, color2, color1_id, color2_id, objects, size = 25):
+    """Draws the different color different shape pairing"""
+
+
+    # Draw and add first object 
+    start, end, center = get_object_position(size, center = positions[3][0])
     cv2.rectangle(img, start, end, color1, -1)
-    cv2.circle(img,positions[3][1], size, color2, -1)
+    objects.append((color1_id, center, 'r', (start[0], start[1], end[0], end[1])))
 
+        # Draw and add first object 
+    start, end, center = get_object_position(size, center = positions[3][1])
+    cv2.circle(img,center, size, color2, -1)
+    objects.append((color2_id, center, 'c', (start[0], start[1], end[0], end[1])))
 
-    return img
+    return img, objects
     
